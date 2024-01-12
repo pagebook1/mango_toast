5f7af2da27981825388c6aa81d5a3f3f213fb894
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2024-01-12 22:57:23 +0800

    Add MangoToast widget for displaying toast messages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,14 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+# MangoToast
+
+MangoToast is a custom Flutter widget that displays a toast message on the screen. It supports different types of messages like success, error, and warning. Each type of message is displayed with a different color.
+
+## Usage
+
+To display a warning message, you can use the `MangoToast.warning` method. This method takes a string message and an optional position parameter. The position parameter determines the alignment of the toast on the screen. It can be `toastPosition.left`, `toastPosition.right`, or `toastPosition.center`.
+
+Here's an example of how to use it:
+
+```dart
+MangoToast.warning('Warning message', position: toastPosition.left).show(context);
--- lib/mango_toast.dart
+++ lib/mango_toast.dart
@@ -1,7 +1,91 @@
 library mango_toast;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+enum toastPosition { left, right, center }
+
+class MangoToast {
+  //check if context is not needed then dispose
+  static MangoToastMessage success(String message,
+      {Duration duration = const Duration(seconds: 2),
+      toastPosition position = toastPosition.center}) {
+    return MangoToastMessage(message, Colors.green, duration, position);
+  }
+
+  static MangoToastMessage error(String message,
+      {Duration duration = const Duration(seconds: 2),
+      toastPosition position = toastPosition.center}) {
+    return MangoToastMessage(message, Colors.red, duration, position);
+  }
+
+  static MangoToastMessage warning(String message,
+      {Duration duration = const Duration(seconds: 2),
+      toastPosition position = toastPosition.center}) {
+    return MangoToastMessage(
+        message, Colors.deepOrangeAccent, duration, position);
+  }
+
+  void dispose(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+  }
+}
+
+class MangoToastMessage {
+  final String message;
+  final Color color;
+  final Duration duration;
+  final toastPosition position;
+  MangoToastMessage(this.message, this.color, this.duration, this.position);
+  void show(BuildContext context,
+      {Duration duration = const Duration(seconds: 2)}) {
+    final screen_height = MediaQuery.of(context).size.height;
+    final screen_width = MediaQuery.of(context).size.width;
+    double left = .30;
+    double right = .30;
+    double top = .15;
+    if (position == toastPosition.left) {
+      left = .05;
+      right = .60;
+    } else if (position == toastPosition.right) {
+      left = .60;
+      right = .05;
+    }
+    if(screen_width < 500){
+      left = .01;
+      right = .01;
+      top = .20;
+    }
+    final scaffold = ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Padding(padding: EdgeInsets.only(left: screen_width * .001)),
+          Text(message, style: const TextStyle(color: Colors.white)),
+          ButtonBar(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.close),
+                color: Colors.white,
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+              ),
+            ],
+          ),
+        ]),
+        backgroundColor: color,
+        duration: duration,
+        behavior: SnackBarBehavior.floating,
+        dismissDirection: DismissDirection.none,
+        closeIconColor: Colors.white,
+        margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).size.height - (screen_height * top),
+            right: screen_width * right,
+            left: screen_width * left),
+        padding: EdgeInsets.only(top: screen_height * .005),
+      ),
+    );
+
+    scaffold.closed.then((value) => MangoToast().dispose(context));
+  }
 }
