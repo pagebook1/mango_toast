f9c304324d282c4c67d135c9b41c56b804f553c2
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2024-01-13 13:41:23 +0800

    Update MangoToast version and add official release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,5 @@
 First Release. Created toast using snackbar. Can be aligned from left to right and also adjust its duration
 ## 0.0.2
 Add supported platforms
+## 1.0.0
+Official Release
--- lib/mango_toast.dart
+++ lib/mango_toast.dart
@@ -25,9 +25,15 @@ class MangoToast {
         message, Colors.deepOrangeAccent, duration, position);
   }
 
-  void dispose(BuildContext context) {
-    ScaffoldMessenger.of(context).clearSnackBars();
+  static MangoToastMessage info(String message,
+      {Duration duration = const Duration(seconds: 2),
+      toastPosition position = toastPosition.center}) {
+    return MangoToastMessage(message, Colors.blueAccent, duration, position);
   }
+
+  // void dispose(BuildContext context) {
+  //   ScaffoldMessenger.of(context).clearSnackBars();
+  // }
 }
 
 class MangoToastMessage {
@@ -38,54 +44,76 @@ class MangoToastMessage {
   MangoToastMessage(this.message, this.color, this.duration, this.position);
   void show(BuildContext context,
       {Duration duration = const Duration(seconds: 2)}) {
-    final screen_height = MediaQuery.of(context).size.height;
-    final screen_width = MediaQuery.of(context).size.width;
-    double left = .30;
-    double right = .30;
-    double top = .15;
-    if (position == toastPosition.left) {
-      left = .05;
-      right = .60;
-    } else if (position == toastPosition.right) {
-      left = .60;
-      right = .05;
+    if (context.mounted) {
+      final screen_height = MediaQuery.of(context).size.height;
+      final screen_width = MediaQuery.of(context).size.width;
+      double left = .30;
+      double right = .30;
+      double top = .15;
+      double fontSize = 15;
+      // make the left and right padding depends on how long the message is
+      if (position == toastPosition.left) {
+        left = .05;
+        right = .60;
+      } else if (position == toastPosition.right) {
+        left = .60;
+        right = .05;
+      }
+      if (screen_width < 500) {
+        left = .01;
+        right = .01;
+        top = .20;
+        fontSize = 10;
+      } else if (screen_width < 800) {
+        left = .05;
+        right = .05;
+        top = .20;
+        fontSize = 12;
+      } else if (screen_width < 1000) {
+        left = .10;
+        right = .10;
+        top = .20;
+        fontSize = 14;
+      }
+      final scaffold = ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Padding(padding: EdgeInsets.only(left: screen_width * .001)),
+            Text(message,
+                style: TextStyle(
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    overflow: TextOverflow.ellipsis)),
+            ButtonBar(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  color: Colors.white,
+                  onPressed: () {
+                    if (context.mounted)
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                  },
+                ),
+              ],
+            ),
+          ]),
+          backgroundColor: color,
+          duration: duration,
+          behavior: SnackBarBehavior.floating,
+          dismissDirection: DismissDirection.none,
+          closeIconColor: Colors.white,
+          margin: EdgeInsets.only(
+              bottom:
+                  MediaQuery.of(context).size.height - (screen_height * top),
+              right: screen_width * right,
+              left: screen_width * left),
+          padding: EdgeInsets.only(top: screen_height * .005),
+        ),
+      );
     }
-    if(screen_width < 500){
-      left = .01;
-      right = .01;
-      top = .20;
-    }
-    final scaffold = ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Padding(padding: EdgeInsets.only(left: screen_width * .001)),
-          Text(message, style: const TextStyle(color: Colors.white)),
-          ButtonBar(
-            children: [
-              IconButton(
-                icon: const Icon(Icons.close),
-                color: Colors.white,
-                onPressed: () {
-                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                },
-              ),
-            ],
-          ),
-        ]),
-        backgroundColor: color,
-        duration: duration,
-        behavior: SnackBarBehavior.floating,
-        dismissDirection: DismissDirection.none,
-        closeIconColor: Colors.white,
-        margin: EdgeInsets.only(
-            bottom: MediaQuery.of(context).size.height - (screen_height * top),
-            right: screen_width * right,
-            left: screen_width * left),
-        padding: EdgeInsets.only(top: screen_height * .005),
-      ),
-    );
 
-    scaffold.closed.then((value) => MangoToast().dispose(context));
+    // scaffold.closed.then((value) => MangoToast().dispose(context));
   }
 }
